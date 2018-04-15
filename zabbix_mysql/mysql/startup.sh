b4df93a232209cb89765856cdd08c8e0d9757ff7
ppabc8 <ppabc8@gmail.com>
2018-04-15 19:18:29 +0800

    update

--- zabbix_mysql/mysql/startup.sh
+++ zabbix_mysql/mysql/startup.sh
@@ -54,4 +54,6 @@ EOF
   rm -f $tfile
 fi
 
-exec /usr/bin/mysqld --user=root --datadir=/data/mysql/data --console
+exec /usr/bin/mysqld --user=root --datadir=/data/mysql/data --console &
+mysql -uroot -D zabbix -p"111111" < "/mysql/zabbix.sql"
+ping 127.0.0.1 >> /dev/null
